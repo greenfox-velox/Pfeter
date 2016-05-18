f59c82c8a9ea5224f61057c1c155869fcdd97098
Pfeter <peter.foldvary@gmail.com>
2016-05-18 14:51:58 +0200

    Add excercises 40 and 41.

---
+++ week-03/day-2/40.py
@@ -0,0 +1,16 @@
+students = [
+        {'name': 'Rezso', 'age': 9.5, 'candies': 2},
+        {'name': 'Gerzson', 'age': 10, 'candies': 1},
+        {'name': 'Aurel', 'age': 7, 'candies': 3},
+        {'name': 'Zsombor', 'age': 12, 'candies': 5}
+]
+# create a function that takes a list of students,
+# then returns how many candies are own by students
+# under 10
+def underten(input_list):
+    underage = 0
+    for i in input_list:
+        if i['age'] < 10:
+            underage += 1
+    return(underage)
+print(underten(students))
---
+++ week-03/day-2/41.py
@@ -0,0 +1,18 @@
+students = [
+        {'name': 'Rezso', 'age': 9.5, 'candies': 2},
+        {'name': 'Gerzson', 'age': 10, 'candies': 1},
+        {'name': 'Aurel', 'age': 7, 'candies': 3},
+        {'name': 'Zsombor', 'age': 12, 'candies': 5},
+        {'name': 'Olaf', 'age': 12, 'candies': 7},
+        {'name': 'Teodor', 'age': 3, 'candies': 2}
+]
+
+# create a function that counts the students that
+# has more than 4 candies
+def morecandies(input_list):
+    candiekings = 0
+    for i in input_list:
+        if i['candies'] > 4:
+            candiekings += 1
+    return(candiekings)
+print(morecandies(students))
