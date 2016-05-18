6d59e9102ec578dfd3d49a296b95fae0b519875f
Pfeter <peter.foldvary@gmail.com>
2016-05-18 14:32:21 +0200

    Refactoring if statements.

--- week-03/day-1/27.py
+++ week-03/day-1/27.py
@@ -2,9 +2,9 @@ z = 13
 # if z is between 10 and 20 print 'Sweet!'
 # if less than 10 print 'More!',
 # if more than 20 print 'Less!'
-if z > 10 and z < 20:
-    print('Sweet!')
+if z > 20:
+    print('Less!')
 elif z < 10:
     print('More!')
-elif z > 20:
-    print('Less!')
+else:
+    print('Sweet!')
