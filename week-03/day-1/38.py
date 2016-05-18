1c5037eb9406e58fecc721d1a3eab3de449aa429
Pfeter <peter.foldvary@gmail.com>
2016-05-18 15:01:17 +0200

    Refactoring for statements

--- week-03/day-1/38.py
+++ week-03/day-1/38.py
@@ -2,13 +2,12 @@
 # But for multiples of three print "Fizz" instead of the number
 # and for the multiples of five print "Buzz".
 # For numbers which are multiples of both three and five print "FizzBuzz".
-
 for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print('FizzBuzz')
-    elif i % 3 == 0:
-        print('Fizz')
-    elif i % 5 == 0:
-        print('Buzz')
-    else:
-        print(i)
+    printable = ''
+    if i % 3 == 0:
+        printable += 'Fizz'
+    if i % 5 == 0:
+        printable += 'Buzz'
+    if printable == '':
+        printable = i
+    print(printable)
