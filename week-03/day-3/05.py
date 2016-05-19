582fac4a48955f218a0759fe09728b319c4eb640
Pfeter <peter.foldvary@gmail.com>
2016-05-19 17:56:49 +0200

    Remove built in methods

--- week-03/day-3/05.py
+++ week-03/day-3/05.py
@@ -9,22 +9,32 @@ class Stack(object):
         self.elements = []
 
     def size(self):
-        return(len(self.elements))
+        elements_size = 0
+        for i in self.elements:
+            elements_size += 1
+        return(elements_size)
 
     def push(self, element):
-        self.elements.append(element)
+        self.elements += element
 
     def pop(self):
-        self.last_element = self.elements[len(self.elements) - 1]
-        self.elements.remove(self.elements[len(self.elements) - 1])
+        elements_size = Stack.size(self)
+        self.last_element = self.elements[elements_size - 1]
+        counter = 0
+        temporary_list = []
+        for shorter_i in self.elements:
+            if counter < (elements_size - 1):
+                temporary_list += [shorter_i]
+                counter += 1
+        self.elements = temporary_list
         return(self.last_element)
 
 stack_1 = Stack()
 print(stack_1.size())
-stack_1.push('thing1')
-stack_1.push('thing2')
-stack_1.push('thing3')
-stack_1.push('thing4')
+stack_1.push(['thing1'])
+stack_1.push(['thing2'])
+stack_1.push(['thing3'])
+stack_1.push(['thing4'])
 print(stack_1.size())
 print(stack_1.pop())
 print(stack_1.size())
