f2a5bc59b5dbcd168e30ff1db0903f192a4b0272
Pfeter <peter.foldvary@gmail.com>
2016-05-20 13:20:43 +0200

    Refactoring and remove unused variable

--- week-03/day-4/palindrome_search.py
+++ week-03/day-4/palindrome_search.py
@@ -3,13 +3,9 @@
 
 def search_palindromes(full_txt):
     palindromes = []
-    for first_pos in range( 0, len(full_txt)):
-        possible_palindrome = ''
-        first_letter = full_txt[first_pos]
-        for second_pos in range(first_pos, len(full_txt)):
-            second_letter = full_txt[second_pos]
-            if (first_letter == second_letter) and (second_pos > first_pos + 1):
-                if full_txt[first_pos: second_pos + 1] == full_txt[second_pos: first_pos - 1: - 1]:
-                    palindromes += [full_txt[first_pos: second_pos + 1]]
+    for first_pos in range(0, len(full_txt)):
+        for second_pos in range(first_pos+2, len(full_txt)):
+            if full_txt[first_pos : second_pos+1] == full_txt[second_pos : first_pos-1 : -1]:
+                palindromes.append(full_txt[first_pos : second_pos+1])
     return(palindromes)
 print(search_palindromes('dog goat dad duck doodle never'))
