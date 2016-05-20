1b95ea106ae5f1fcc37a31f56ea31155995b2bf4
Pfeter <peter.foldvary@gmail.com>
2016-05-20 11:55:11 +0200

    Refactoring palindrome search. Replace reverse_text function to [::-1]

--- week-03/day-4/palindrome_search.py
+++ week-03/day-4/palindrome_search.py
@@ -1,12 +1,7 @@
 # output = search_palindromes('dog goat dad duck doodle never')
 # print(output) # it prints: ['og go', ' dad ', 'd d', 'dood', 'eve']
-def reverse_text(text_input):
-    reversed_text = ''
-    for i in range(len(text_input)-1, -1, -1):
-        reversed_text += text_input[i]
-    return(reversed_text)
 
-def search_palindromes(input_full_text):
+def search_palindromes2(input_full_text):
     palindromes = []
     for first_letter_position in range( 0, len(input_full_text)):
         possible_palindrome = ''
@@ -15,7 +10,7 @@ def search_palindromes(input_full_text):
             second_letter = input_full_text[second_letter_position]
             if (first_letter == second_letter) and (second_letter_position > first_letter_position + 1):
                 possible_palindrome = input_full_text[first_letter_position: second_letter_position + 1]
-                if reverse_text(possible_palindrome) == possible_palindrome:
+                if possible_palindrome == possible_palindrome[::-1]:
                     palindromes += [possible_palindrome]
     return(palindromes)
-print(search_palindromes('dog goat dad duck doodle never indulagorogaludni'))
+print(search_palindromes2('dog goat dad duck doodle never'))
