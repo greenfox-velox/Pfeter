0a2282a16ce5cea4026f76bb12d6c0b79883e8d7
Pfeter <peter.foldvary@gmail.com>
2016-05-22 23:22:35 +0200

    Refactoring

--- week-03/day-1/25.py
+++ week-03/day-1/25.py
@@ -2,7 +2,4 @@ x = 'cheese'
 # Tell if the x has even or odd number of
 # characters with a True for even and
 # false False output otherwise
-if len(x) % 2 == 0:
-    print('True')
-else:
-    print('False')
+print(len(x) % 2 == 0)
