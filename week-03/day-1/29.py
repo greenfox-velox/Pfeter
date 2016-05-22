f8668810adccec2681aaa9dad292c131ddb2efe9
Pfeter <peter.foldvary@gmail.com>
2016-05-22 23:26:56 +0200

    Use is_bonus as boolean itself

--- week-03/day-1/29.py
+++ week-03/day-1/29.py
@@ -1,14 +1,15 @@
 ab = 123
-credits = 1000
-is_bonus = False
+credits = 220
+is_bonus = True
 # if credits are at least 50,
 # and is_bonus is False decrement ab by 2
 # if credits are smaller than 50,
 # and is_bonus is False decrement ab by 1
 # if is_bonus is True ab should remain the same
-if credits > 50 and is_bonus == False:
+if credits > 50 and not is_bonus:
     ab -= 2
-elif credits < 50 and is_bonus == False:
+elif credits < 50 and not is_bonus:
     ab -= 1
-elif is_bonus == True:
+elif is_bonus:
     ab = ab
+print(ab)
