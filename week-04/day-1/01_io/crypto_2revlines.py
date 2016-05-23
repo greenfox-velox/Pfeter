e7b369aad05d75c2da1e75bf841196cb1555ceb1
Pfeter <peter.foldvary@gmail.com>
2016-05-23 14:42:29 +0200

    Add crypto_2revlines excercise

---
+++ week-04/day-1/01_io/crypto_2revlines.py
@@ -0,0 +1,10 @@
+# Create a method that decrypts texts/reversed_zen_lines.txt
+def decrypt(file_name):
+    f = open(file_name)
+    simple_texts = ''
+    for line in f:
+        simple_text = ''
+        for i in range(len(line.rstrip()) - 1, - 1, - 1):
+            simple_text += line.rstrip()[i]
+        simple_texts += simple_text + '\n'
+    return simple_texts
