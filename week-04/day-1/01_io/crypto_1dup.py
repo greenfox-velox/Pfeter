026bc996f06e297f762d1dd78f9194af72e9b687
Pfeter <peter.foldvary@gmail.com>
2016-05-23 14:41:46 +0200

    Add crypto_1dup excercise

---
+++ week-04/day-1/01_io/crypto_1dup.py
@@ -0,0 +1,12 @@
+# Create a method that decrypts the texts/duplicated_chars.txt
+
+def decrypt(file_name):
+    f = open(file_name)
+    simple_texts = ''
+    for line in f:
+        simple_text = ''
+        for i in range(0, len(line), 2):
+            if (i < len(line) - 1):
+                simple_text += line[i]
+        simple_texts += simple_text + '\n'
+    return simple_texts
