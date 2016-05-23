b8d5f22e2988cde0018cfa2fad41275a82545128
Pfeter <peter.foldvary@gmail.com>
2016-05-23 15:30:39 +0200

    Add crypto_4encoded excercise

--- week-04/day-1/01_io/crypto_4encoded.py
+++ week-04/day-1/01_io/crypto_4encoded.py
@@ -1,3 +1,14 @@
 # Create a method that decrypts texts/encoded_zen_lines.txt
 def decrypt(file_name):
-    pass
+    f = open(file_name)
+    simple_texts = ''
+    for line in (f.readlines()):
+        simple_text = ''
+        for i in range(0, len(line.rstrip())):
+            if line.rstrip()[i] == ' ':
+                simple_text += ' '
+            else:
+                simple_text += chr(ord(line.rstrip()[i])-1)
+        simple_texts += simple_text + '\n'
+    f.close()
+    return simple_texts
