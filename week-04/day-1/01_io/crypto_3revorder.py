599089fb51fcdbaa726734d65e0299e52acb83fd
Pfeter <peter.foldvary@gmail.com>
2016-05-23 15:14:41 +0200

    Add crypto_3revorder excercise

--- week-04/day-1/01_io/crypto_3revorder.py
+++ week-04/day-1/01_io/crypto_3revorder.py
@@ -1,3 +1,10 @@
 # Create a method that decrypts texts/reversed_zen_order.txt
 def decrypt(file_name):
-    pass
+    f = open(file_name)
+    simple_texts = ''
+    for line in reversed(f.readlines()):
+        simple_text = ''
+        for i in range(0, len(line.rstrip())):
+            simple_text += line.rstrip()[i]
+        simple_texts += simple_text + '\n'
+    return simple_texts
