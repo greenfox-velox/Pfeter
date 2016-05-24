af37fcf34457730064d66fe15c9de1ae51df49ce
Pfeter <peter.foldvary@gmail.com>
2016-05-24 22:46:56 +0200

    Use range instead of manual tracking of the index

--- week-03/day-2/linear_search_2.py
+++ week-03/day-2/linear_search_2.py
@@ -1,12 +1,9 @@
 # linear_search([4,5,7], 6)
 # expected output: -1
 def linear_search(input_list, target):
-    output = 0
-    for i in input_list:
-        if i == target:
-            return(output)
-        elif output == len(input_list) -1:
+    for i in range(0, len(input_list)):
+        if input_list[i] == target:
+            return(i)
+        elif i == len(input_list) -1:
             return("-1")
-        else:
-            output += 1
 print(linear_search([4,5,7], 6))
