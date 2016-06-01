3c3ddd46c9c016db391b1d6a4c4c148240192fbe
Pfeter <peter.foldvary@gmail.com>
2016-06-01 13:35:26 +0200

    Add test cases of the number_of_lines function

--- week-05/day-3/test.py
+++ week-05/day-3/test.py
@@ -1,5 +1,6 @@
 import unittest
 from divide_ten import divide_ten
+from number_of_lines import number_of_lines
 
 class TestDivideTen(unittest.TestCase):
 
@@ -14,6 +15,12 @@ class TestDivideTen(unittest.TestCase):
 
     def test_divide_by_zero(self):
         self.assertEqual(divide_ten(0), 'fail')
+
+    def test_read_number_of_lines(self):
+        self.assertEqual(number_of_lines('test.txt'), 3)
+
+    def test_read_file_not_exist(self):
+        self.assertEqual(number_of_lines('not_test.txt'), 0)
 
 if __name__ == '__main__':
     unittest.main()
