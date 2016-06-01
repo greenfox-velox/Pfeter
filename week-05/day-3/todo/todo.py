7711dde3cf85ee739c2efe944f5cd08ae35770f1
Pfeter <peter.foldvary@gmail.com>
2016-06-01 15:40:23 +0200

    Add List Story

--- week-05/day-3/todo/todo.py
+++ week-05/day-3/todo/todo.py
@@ -1,3 +1,5 @@
+import sys
+
 def usage_information():
     print('Python Todo application')
     print('=======================')
@@ -5,7 +7,26 @@ def usage_information():
     print('Command line arguments:')
     print(' -l   Lists all the tasks')
     print(' -a   Adds a new task')
-    print(' -r   Removes an task')
-    print(' -c   Completes an task')
+    print(' -r   Removes a task')
+    print(' -c   Completes a task')
 
-usage_information()
+def read_todos(file_name):
+        output = []
+        f = open(file_name)
+        for line in f:
+            output.append(line.rstrip())
+        f.close()
+        return output
+
+def list_todos(file_name):
+    todos = read_todos(file_name)
+    for i in range(len(todos)):
+        print(i + 1,'-', todos[i])
+
+def main():
+    if len(sys.argv) == 1:
+        usage_information()
+    elif sys.argv[1] == '-l':
+        list_todos('todos.txt')
+
+main()
