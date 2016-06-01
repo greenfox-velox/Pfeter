af96d1dd769d74f04d6a1ff8c5c3dca4e716671b
Pfeter <peter.foldvary@gmail.com>
2016-06-01 16:39:52 +0200

    Add the Add task error handling Story

--- week-05/day-3/todo/todo.py
+++ week-05/day-3/todo/todo.py
@@ -28,9 +28,15 @@ def list_todos(file_name):
 
 def append_todos(file_name, new_todo):
         output = []
-        f = open(file_name, "a")
+        f = open(file_name, 'a')
         f.write(new_todo + '\n')
         f.close()
+
+def add_new_task(file_name):
+    if len(sys.argv) == 3:
+        append_todos(file_name, sys.argv[2])
+    else:
+        print('Unable to add: No task is provided')
 
 def main():
     if len(sys.argv) == 1:
@@ -38,6 +44,6 @@ def main():
     elif sys.argv[1] == '-l':
         list_todos('todos.txt')
     elif sys.argv[1] == '-a':
-        append_todos('todos.txt', sys.argv[2])
+        add_new_task('todos.txt')
 
 main()
