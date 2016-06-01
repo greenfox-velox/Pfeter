556f9ef3a8b92572a92033b2b5f3193bc1ed6c47
Pfeter <peter.foldvary@gmail.com>
2016-06-01 14:32:45 +0200

    Add Usage Storie

---
+++ week-05/day-3/todo/todo.py
@@ -0,0 +1,11 @@
+def usage_information():
+    print('Python Todo application')
+    print('=======================')
+    print('')
+    print('Command line arguments:')
+    print(' -l   Lists all the tasks')
+    print(' -a   Adds a new task')
+    print(' -r   Removes an task')
+    print(' -c   Completes an task')
+
+usage_information()
