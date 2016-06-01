0d59273ba70dbe7eb54fc7abc1fbda6016703596
Pfeter <peter.foldvary@gmail.com>
2016-06-01 16:22:09 +0200

    Add the Add new task Story

--- week-05/day-3/todo/todo.py
+++ week-05/day-3/todo/todo.py
@@ -26,10 +26,18 @@ def list_todos(file_name):
     else:
         print('No todos for today! :)')
 
+def append_todos(file_name, new_todo):
+        output = []
+        f = open(file_name, "a")
+        f.write(new_todo + '\n')
+        f.close()
+
 def main():
     if len(sys.argv) == 1:
         usage_information()
     elif sys.argv[1] == '-l':
         list_todos('todos.txt')
+    elif sys.argv[1] == '-a':
+        append_todos('todos.txt', sys.argv[2])
 
 main()
