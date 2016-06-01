8010a31c2af3b7a6e1b1fb3e4fcedb2c1c05a81e
Pfeter <peter.foldvary@gmail.com>
2016-06-01 16:54:50 +0200

    Correct the intendation

--- week-05/day-3/todo/todo.py
+++ week-05/day-3/todo/todo.py
@@ -11,12 +11,12 @@ def usage_information():
     print(' -c   Completes a task')
 
 def read_todos(file_name):
-        output = []
-        f = open(file_name)
-        for line in f:
-            output.append(line.rstrip())
-        f.close()
-        return output
+    output = []
+    f = open(file_name)
+    for line in f:
+        output.append(line.rstrip())
+    f.close()
+    return output
 
 def list_todos(file_name):
     todos = read_todos(file_name)
@@ -27,16 +27,22 @@ def list_todos(file_name):
         print('No todos for today! :)')
 
 def append_todos(file_name, new_todo):
-        output = []
-        f = open(file_name, 'a')
-        f.write(new_todo + '\n')
-        f.close()
+    output = []
+    f = open(file_name, 'a')
+    f.write(new_todo + '\n')
+    f.close()
 
 def add_new_task(file_name):
     if len(sys.argv) == 3:
         append_todos(file_name, sys.argv[2])
     else:
         print('Unable to add: No task is provided')
+
+def remove_todos(file_name):
+    output = []
+    f = open(file_name, 'a')
+    f.write(new_todo + '\n')
+    f.close()
 
 def main():
     if len(sys.argv) == 1:
