2d77528927e1166e35c61fa7a567448cf38faa24
Pfeter <peter.foldvary@gmail.com>
2016-06-27 16:04:41 +0200

    Add the solutions of 16-20 excercises

---
+++ week-09/day-1/16.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var p1 = [1, 2, 3];
+var p2 = [4, 5];
+// tell if p2 has more elements than p1
+if (p1.length < p2.length){
+    console.log('p2 has more elements than p1')
+}
---
+++ week-09/day-1/20.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var t = [1, 2, 3, 4, 5];
+// increment the 3rd element
+t[2] += 1
+
+console.log(t[2])
