1e83d733d8f06923f9a2d645598c1540e770c88f
Pfeter <peter.foldvary@gmail.com>
2016-06-30 16:42:50 +0200

    Add big picture paging.

--- week-09/project-gallery/script/gallery.js
+++ week-09/project-gallery/script/gallery.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const thumbnailPicsUrls = [
+const picsUrls = [
   'images/1.jpg',
   'images/2.jpg',
   'images/3.jpg',
@@ -12,7 +12,26 @@ const thumbnailPicsUrls = [
   'images/9.jpg',
   'images/10.jpg',
 ];
-const thumbnailPics = document.querySelectorAll('.thumbnailpic');
-for (let i = 0; i < thumbnailPics.length; i++) {
-  thumbnailPics[i].src = thumbnailPicsUrls[i];
+const leftArrow = document.querySelector('.arrow.left');
+const rightArrow = document.querySelector('.arrow.right');
+
+let bigPictureIndex = 0;
+document.querySelector('.bigpicture').src = picsUrls[bigPictureIndex];
+
+function nextPicture() {
+  if (bigPictureIndex < picsUrls.length - 1) {
+    bigPictureIndex++;
+    document.querySelector('.bigpicture').src = picsUrls[bigPictureIndex];
+  }
 }
+
+function previousPicture() {
+  if (bigPictureIndex > 0) {
+    bigPictureIndex--;
+    document.querySelector('.bigpicture').src = picsUrls[bigPictureIndex];
+  }
+}
+
+rightArrow.addEventListener('click', nextPicture);
+
+leftArrow.addEventListener('click', previousPicture);
