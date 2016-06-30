c0e7ab549eb6bf98b02048dfa9c106c0436f9f21
Pfeter <peter.foldvary@gmail.com>
2016-06-30 11:41:57 +0200

    Basic galery.

---
+++ week-09/project-gallery/script/gallery.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const thumbnailPicsUrls = [
+  'images/1.jpg',
+  'images/2.jpg',
+  'images/3.jpg',
+  'images/4.jpg',
+  'images/5.jpg',
+  'images/6.jpg',
+  'images/7.jpg',
+  'images/8.jpg',
+  'images/9.jpg',
+  'images/10.jpg',
+];
+const thumbnailPics = document.querySelectorAll('.thumbnailpic');
+for (let i = 0; i < thumbnailPics.length; i++) {
+  thumbnailPics[i].src = thumbnailPicsUrls[i];
+}
