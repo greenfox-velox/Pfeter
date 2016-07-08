7cd645a062128358f17a2db62d5806d5af1efce2
Pfeter <peter.foldvary@gmail.com>
2016-07-08 17:10:35 +0200

    Use list instead of just labels. Make function from xhr.onload. Add addTodo function. Add eventListeners to process the user requests. Correct the h1 closing tag.

--- week-08/project-todos/script/script.js
+++ week-08/project-todos/script/script.js
@@ -1,29 +1,53 @@
 'use strict';
 
-const xhr = new XMLHttpRequest();
-
-function todosToList(newElement) {
-  console.log(newElement);
-  const newLabel = document.createElement('label');
+function showTodo(newElement) {
+  const newLi = document.createElement('li');
   const newInput = document.createElement('input');
+  const newLabel = document.createElement('label');
   const newImg = document.createElement('img');
-  document.querySelector('.todo-item').appendChild(newLabel);
+  document.querySelector('.todo-items').appendChild(newLi);
+  newLi.appendChild(newLabel);
   newLabel.textContent = newElement.text;
   newLabel.id = newElement.id;
-  newLabel.appendChild(newImg);
+  newLi.appendChild(newImg);
   newImg.id = newElement.id;
   newImg.src = 'image/trash.png';
   newImg.classList.add('trash');
-  newLabel.appendChild(newInput);
+  newLi.appendChild(newInput);
   newInput.type = 'checkbox';
-  newInput.value = newElement.id;
+  newInput.id = newElement.id;
+  newInput.checked = newElement.completed;
 }
 
-xhr.onload = function () {
-  JSON.parse(xhr.response).forEach(function (e) {
-    todosToList(e);
-  });
-};
+function pageOnload() {
+  const xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    JSON.parse(xhr.response).forEach(function (e) {
+      showTodo(e);
+    });
+  };
+  xhr.open('GET', 'https://mysterious-dusk-8248.herokuapp.com/todos');
+  xhr.send();
+}
 
-xhr.open('GET', 'https://mysterious-dusk-8248.herokuapp.com/todos');
-xhr.send();
+function addTodo() {
+  const xhr = new XMLHttpRequest();
+  const inputText = document.querySelector('input').value;
+  const allLi = document.querySelectorAll('li label');
+  const nextId = parseInt(allLi[allLi.length - 1].id) + 1;
+  const inputValue = { id: nextId, text: inputText, completed: false };
+  xhr.open('POST', 'https://mysterious-dusk-8248.herokuapp.com/todos');
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.send(JSON.stringify(inputValue));
+  showTodo(inputValue);
+}
+
+document.querySelector('.add-button').addEventListener('click', function () {
+  addTodo();
+});
+
+document.querySelector('ul').addEventListener('click', function () {
+  console.log(event.target.id);
+});
+
+pageOnload();
