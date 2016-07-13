10a90e0ff65c60e899a1ef71f798773eeddf8990
Pfeter <peter.foldvary@gmail.com>
2016-07-13 23:01:53 +0200

    Add basic public frontend. Add partial mysql query to backend.

---
+++ week-09/todo/public/script/script.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var url = 'http://localhost:3000/todos';
+
+function showTodo(newElement) {
+  const newLi = document.createElement('li');
+  const newInput = document.createElement('input');
+  const newLabel = document.createElement('label');
+  const newImg = document.createElement('img');
+  document.querySelector('.todo-items').appendChild(newLi);
+  newLi.appendChild(newLabel);
+  newLabel.textContent = newElement.text;
+  newLabel.id = newElement.id;
+  newLi.appendChild(newImg);
+  newImg.id = newElement.id;
+  newImg.src = 'image/trash.png';
+  newImg.classList.add('trash');
+  newLi.appendChild(newInput);
+  newInput.type = 'checkbox';
+  newInput.id = newElement.id;
+  newInput.checked = newElement.completed;
+}
+
+function pageOnload() {
+  const xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    JSON.parse(xhr.response).forEach(function (e) {
+      showTodo(e);
+    });
+  };
+  xhr.open('GET', 'http://localhost:3000/todos');
+  xhr.send();
+}
+
+function addTodo() {
+  const xhr = new XMLHttpRequest();
+  const inputText = document.querySelector('input').value;
+  const allLi = document.querySelectorAll('li label');
+  const nextId = parseInt(allLi[allLi.length - 1].id) + 1;
+  const inputValue = { id: nextId, text: inputText, completed: false };
+  xhr.open('POST', 'http://localhost:3000/todos');
+  xhr.setRequestHeader('content-type', 'application/json');
+  xhr.send(JSON.stringify(inputValue));
+  showTodo(inputValue);
+}
+
+document.querySelector('.add-button').addEventListener('click', function () {
+  addTodo();
+});
+
+document.querySelector('ul').addEventListener('click', function () {
+  console.log(event.target.id);
+});
+
+pageOnload();
--- week-09/todo/todo_server_app.js
+++ week-09/todo/todo_server_app.js
@@ -4,55 +4,66 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
-const data = [
-  {
-    id: 1,
-    text: 'Buy milk',
-    completed: false,
-  },
-  {
-    id: 2,
-    text: 'Make dinner',
-    completed: false,
-  },
-  {
-    id: 3,
-    text: 'Save the world',
-    completed: false,
-  },
-];
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: '*',
+  user: '*',
+  password: '*',
+  database: '*',
+});
+
+con.connect(function (err) {
+  if (err) {
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
 
 app.get('/todos', function (req, res) {
-  res.json(data);
+  con.query('SELECT * FROM todos;',function(err, data){
+    if(err) {
+      console.log(err.toString());
+      return;
+    }
+    res.json(data);
+  });
 });
 
-app.post('/todos', function (req, res) {
-  const newTodo = {
-    completed: false,
-    id: data.length + 1,
-    text: req.body.text,
-  };
-  data.push(newTodo);
-  res.json(newTodo);
-});
+// con.query('INSERT INTO todos (text, completed) VALUES (?, 'false');' req.body.text, funtion....
+
+// app.post('/todos', function (req, res) {
+//   const newTodo = {
+//     completed: false,
+//     id: data.length + 1, // helyette a szervertől kellene lekérdezni az id-t
+//     text: req.body.text,
+//   };
+//   data.push(newTodo);
+//   res.json(newTodo);
+// });
 
 app.get('/todos/:id', function (req, res) {
-  res.json(data.filter(function (e) {
-    if (e.id === +req.params.id) {
-      return e;
+  con.query('SELECT * FROM todos WHERE todos.id = ?', req.params.id, function (err, data){
+    if(err) {
+      console.log(err.toString());
+      return;
     }
-  })[0]);
+    res.json(data[0]);
+  });
 });
 
 app.put('/todos/:id', function (req, res) {
-  res.json(data.filter(function (e) {
-    if (e.id === +req.params.id) {
-      e.text = req.body.text;
-      e.completed = true;
-      return e;
+  con.query('UPDATE todos SET completed = "true" WHERE id = ?', req.params.id, function (err, data){
+    if(err) {
+      console.log(err.toString());
+      return;
     }
-  })[0]);
+    con.
+    res.json(data[0]);
+  });
 });
 
 app.delete('/todos/:id', function (req, res) {
@@ -63,6 +74,11 @@ app.delete('/todos/:id', function (req, res) {
       return e;
     }
   })[0]);
+});
+
+app.use(function (err, req, res, next) {
+  res.sendStatus(404);
+  next();
 });
 
 app.listen(3000);
